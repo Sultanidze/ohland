e2bc2ca1a684d991ad6e62800896fc2b81d1be9c
Sultanidze <Sultanidze@gmail.com>
2017-04-24 18:52:55 +0300

    before responses slider construction

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 	$(".b-vehicle").click(function(){
 		$(".b-vehicle").removeClass("b-vehicle_active");
 		$(this).addClass("b-vehicle_active");
-		$("#" + $(this).attr("data-id")).trigger("click");	//перемикання радіобатонів об’єму при кліку на тз
+		// $("#" + $(this).attr("data-id")).trigger("click");	//РїРµСЂРµРјРёРєР°РЅРЅСЏ СЂР°РґС–РѕР±Р°С‚РѕРЅС–РІ РѕР±вЂ™С”РјСѓ РїСЂРё РєР»С–РєСѓ РЅР° С‚Р·
 	});
 // reasons slider
 	var $sliderReasons = $(".b-reasons__slider .b-slider__string").slick({
@@ -29,4 +29,72 @@ $(document).ready(function(){
 		event.preventDefault();
 		$sliderReasons.slick("slickNext");
 	});
+
+// clientsRating/mtsb switch
+	var  $ratingBtns = $(".b-section_rating .b-rating__btn")
+		,$ratingClients = $(".b-rating__wrapper_clients")
+		,$ratingMtsbu = $(".b-rating__wrapper_mtsbu")
+		;
+	$ratingBtns.click(function(){
+		if (!$(this).hasClass("b-rating__btn_active")){
+			$ratingBtns.removeClass("b-rating__btn_active");
+			$(this).addClass("b-rating__btn_active")
+			if($(this).hasClass("b-rating__btn_mtsbu")){
+				$ratingClients.fadeOut(600);
+				$ratingMtsbu.fadeIn(600);
+			} else if($(this).hasClass("b-rating__btn_clients")){
+				$ratingMtsbu.fadeOut(600);
+				$ratingClients.fadeIn(600);
+			}
+		}
+	});
+
+//	scroll to calculator
+	$(".b-anchor_calculator").click(function(event){
+		event.preventDefault();
+		var  $anchor = $($(this).attr("href"))
+			,offsetAnchor = $anchor.offset().top
+			;
+		console.log(offsetAnchor);
+		$('html, body').animate({ scrollTop: offsetAnchor}, 600);
+		return false;
+	});
+
+// client rating slider
+	var $sliderRating = $(".b-rating__slider .b-slider__string_rating").slick({
+		arrows: false,
+		infinite: true,
+		speed: 400,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		// autoplay: true,
+		// autoplaySpeed: 4400
+	});
+	// add my buttons to ads slider
+	var  $sliderRatingPrevBtn = $sliderRating.parents(".b-rating__slider").find(".b-slider__control_left")
+		,$sliderRatingNextBtn = $sliderRating.parents(".b-rating__slider").find(".b-slider__control_right")
+		;
+	$sliderRatingPrevBtn.click(function(event){
+		event.preventDefault();
+		$sliderRating.slick("slickPrev");
+	});	
+	$sliderRatingNextBtn.click(function(event){
+		event.preventDefault();
+		$sliderRating.slick("slickNext");
+	});
+
+// faq accordion
+	var  $faq = $(".b-questAnsw")
+		,$faqHeading = $(".b-questAnsw h5")
+		;
+
+		$(".b-questAnsw:nth-child(n+2)").find("p").hide();
+		$faqHeading.click(function(){
+			if ($(this).parent(".b-questAnsw").hasClass("b-questAnsw_unrolled")){
+				$(this).next("p").slideUp(400).parent(".b-questAnsw").removeClass("b-questAnsw_unrolled");
+			} else {
+				$faq.find(".b-questAnsw_unrolled").next("p").slideUp(400).parent(".b-questAnsw").removeClass("b-questAnsw_unrolled");
+				$(this).next("p").slideDown(400).parent(".b-questAnsw").addClass("b-questAnsw_unrolled");
+			}
+		})
 });
